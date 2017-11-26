656062ae1cafa2ce5a00dfb381dedcd9e2823f9c
Yassine Chbani <yassinec@bam.tech>
2017-11-26 12:41:06 +0100

    Tweak puzzle 23

--- src/advent16/Day23.java
+++ src/advent16/Day23.java
@@ -6,83 +6,93 @@ import java.util.Map;
 
 public class Day23 extends tools{
 
-	private static void Run(List<String> inFile, Map<String,Integer> map, int i) {
-		while(i>=0 && i<inFile.size()){
-			String comm = inFile.get(i);
-			println(comm);
-			String[] elements = comm.split(" ");
-			String inst = elements[0];
+	private static void increment(Map<String,Integer> map, String[] elements, int inc) {
+		String register = elements[1];
+		int newValue = map.get(register) + inc;
+		map.put(register, newValue);
+ }
 
-			if(inst.equals("tgl")) {
+ private static void copy(Map<String,Integer> map, String[] elements) {
+	String register = elements[2];
+	if(!tools.isInteger(register)) {
+		int newValue = tools.isInteger(elements[1]) ? Integer.parseInt(elements[1]) : map.get(elements[1]); 
+		map.put(register, newValue);
+	}
+ }
+
+ private static void toggle(List<String> inputFile, int i, Map<String,Integer> map, String[] elements) {
 				int position = tools.isInteger(elements[1]) ? Integer.parseInt(elements[1]) : map.get(elements[1]); 
-				if(i + position>=0 && i + position<inFile.size()) {
-					String toToggle = inFile.get(i + position);
-					String[] elts = toToggle.split(" ");
-					String oldInst = elts[0];
-					String newInst = oldInst;
+		if(i + position>=0 && i + position<inputFile.size()) {
+			String toToggle = inputFile.get(i + position);
+			String[] toToggleElements = toToggle.split(" ");
+			String oldInstruction = toToggleElements[0];
+			String newInstruction = oldInstruction;
 
-					if(elts.length==2) { // one-argument instruction
-						newInst = oldInst.equals("inc") ? "dec" : "inc";
+		if(toToggleElements.length==2) { // one-argument instruction
+			newInstruction = oldInstruction.equals("inc") ? "dec" : "inc";
 					}
-					else if(elts.length==3) { // two-argument instruction
-						newInst = oldInst.equals("jnz") ? "cpy" : "jnz";
+		else if(toToggleElements.length==3) { // two-argument instruction
+			newInstruction = oldInstruction.equals("jnz") ? "cpy" : "jnz";
 					}
-					String newToggled = toToggle.replace(oldInst, newInst);
-					inFile.set(i + position, newToggled);
+		String newToggled = toToggle.replace(oldInstruction, newInstruction);
+		inputFile.set(i + position, newToggled);
 				}
 			}
 
-			if(inst.equals("cpy")){
-				String register0 = elements[2];
-				if(!tools.isInteger(register0)) {
-					int newValue0 = tools.isInteger(elements[1]) ? Integer.parseInt(elements[1]) : map.get(elements[1]); 
-					map.put(register0, newValue0);
+	private static void Run(List<String> inputFile, Map<String,Integer> map, int i) {
+		while(i>=0 && i<inputFile.size()){
+			String line = inputFile.get(i);
+			String[] elements = line.split(" ");
+			String instruction = elements[0];
+
+			if(instruction.equals("tgl")) {
+				toggle(inputFile, i, map, elements);
 				}
+
+			if(instruction.equals("cpy")){
+				copy(map, elements);
 			}
 
-			else if(inst.equals("inc")){
-				String register1 = elements[1];
-				int newValue1 = map.get(register1) + 1;
-				map.put(register1, newValue1);
+			else if(instruction.equals("inc")){
+				increment(map, elements, 1);
 			}
 
-			else if(inst.equals("dec")){
-				String register2 = elements[1];
-				int newValue2 = map.get(register2) - 1;
-				map.put(register2, newValue2);
+			else if(instruction.equals("dec")){
+				increment(map, elements, -1);
 			}
 
-			else if(inst.equals("jnz")){
-				//				println(map.get("d"));
-				String register3 = elements[1];
-				if(i<inFile.size() - 2){
-					String[] inOneStep = inFile.get(i+1).split(" ");
-					String[] inTwoSteps = inFile.get(i+2).split(" ");
-					String[] threeStepsAgo = inFile.get(i-3).split(" ");
+			else if(instruction.equals("jnz")){
+				String register = elements[1];
+				if(i<inputFile.size() - 2){
+					String[] inOneStep = inputFile.get(i+1).split(" ");
+					String[] inTwoSteps = inputFile.get(i+2).split(" ");
+					String[] threeStepsAgo = inputFile.get(i-3).split(" ");
 
 					// Big optimisation
 					if(threeStepsAgo[0].equals("cpy") && inOneStep[0].equals("dec") && inTwoSteps[0].equals("jnz") && inOneStep[1].equals(inTwoSteps[1])) {
-						String targetRegister = inFile.get(i-2).split(" ")[1];
-						String mult1 = inFile.get(i-3).split(" ")[1];
-						String mult2 = inFile.get(i+1).split(" ")[1]; // to set to 0
-						String toZero = inFile.get(i-1).split(" ")[1];
-						map.put(targetRegister, map.get(mult1)*map.get(mult2));
+						String targetRegister = inputFile.get(i-2).split(" ")[1];
+						String mult1 = threeStepsAgo[1];
+						String mult2 = inOneStep[1]; // to set to 0
+						String toZero = inputFile.get(i-1).split(" ")[1];
+						int mult1Value = tools.isInteger(mult1) ? Integer.valueOf(mult1) : map.get(mult1);
+						map.put(targetRegister, (map.get(targetRegister)-1)+(mult1Value+0)*map.get(mult2));
 						map.put(mult2, 0);
 						map.put(toZero, 0);
 						i += 3;
+						continue;
 					}
 
 					else { // Small optimisation
-						String registerOne = inFile.get(i-2).split(" ")[0];
-						String registerTwo = inFile.get(i-1).split(" ")[0];
+						String registerOne = inputFile.get(i-2).split(" ")[0];
+						String registerTwo = inputFile.get(i-1).split(" ")[0];
 						Map<String, String> loopMap = new HashMap<String, String>();
 
-						loopMap.put(registerOne, inFile.get(i-2).split(" ")[1]);
-						loopMap.put(registerTwo, inFile.get(i-1).split(" ")[1]);
+						loopMap.put(registerOne, inputFile.get(i-2).split(" ")[1]);
+						loopMap.put(registerTwo, inputFile.get(i-1).split(" ")[1]);
 
 
 						boolean incAndDec = loopMap.containsKey("inc") && loopMap.containsKey("dec");
-						boolean same = incAndDec && loopMap.get("dec").equals(register3);
+						boolean same = incAndDec && loopMap.get("dec").equals(register);
 
 						if(same){
 							String superReg = loopMap.get("inc");
@@ -95,7 +105,7 @@ public class Day23 extends tools{
 						}
 					}
 				}
-				int testValue = tools.isInteger(register3) ? Integer.valueOf(register3) : map.get(register3);
+				int testValue = tools.isInteger(register) ? Integer.valueOf(register) : map.get(register);
 
 				if(testValue!=0){
 					int increment = tools.isInteger(elements[2]) ? Integer.parseInt(elements[2]) : map.get(elements[2]);
@@ -124,9 +134,9 @@ public class Day23 extends tools{
 
 		println("Part 1: register 'a' has value " + map.get("a"));
 
-		//		Run(inFile2, map2, 0);
-		//
-		//		println("Part 2: register 'a' has value " + map2.get("a"));
+		Run(inFile2, map2, 0);
+		
+		println("Part 2: register 'a' has value " + map2.get("a"));
 
 	}
 
