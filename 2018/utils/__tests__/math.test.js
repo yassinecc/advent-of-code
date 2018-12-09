97f7a7b257c9e27adcf797337a74ba6ee3fe7929
Yassine Chbani <yassine.chbani@gmail.com>
2018-12-09 15:17:37 +0100

    Update test

--- 2018/utils/__tests__/math.test.js
+++ 2018/utils/__tests__/math.test.js
@@ -4,6 +4,6 @@ describe('Day3', () => {
   it('should correctly multiply arrays', () => {
     const c = [[2, 0], [-1, 3]];
     const d = [[7, 1], [-2, 3]];
-    expect(arraySum(c, d)).toEqual([[14, 0], [2, 9]]);
+    expect(arraySum(c, d)).toEqual([[9, 1], [-3, 6]]);
   });
 });
