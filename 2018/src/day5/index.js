d93aa84835a463e0bd38f10ba19e0d5d6d1f41ad
Yassine Chbani <yassine.chbani@gmail.com>
2018-12-25 22:47:56 +0100

    Start of day

---
+++ 2018/src/day5/index.js
@@ -0,0 +1,2 @@
+const { parseFile } = require('../../utils/common');
+const { part1, part2 } = require('../day5/parts');
---
+++ 2018/src/day5/parts.js
@@ -0,0 +1,7 @@
+const part1 = () => {
+  return 0;
+};
+const part2 = () => {
+  return 0;
+};
+module.exports = { part1, part2 };
