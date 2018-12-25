40a98b8bb90bcf5e3b5c45a463dfc209e66cf14b
Yassine Chbani <yassine.chbani@gmail.com>
2018-12-25 23:55:33 +0100

    part1

--- 2018/src/day5/index.js
+++ 2018/src/day5/index.js
@@ -1,2 +1,6 @@
 const { parseFile } = require('../../utils/common');
 const { part1, part2 } = require('../day5/parts');
+
+const word = parseFile('inputs/day5.txt');
+
+console.log("Final polymer's length is", part1(word[0]));
--- 2018/src/day5/parts.js
+++ 2018/src/day5/parts.js
@@ -1,7 +1,30 @@
-const part1 = () => {
-  return 0;
+const { findIndex } = require('lodash');
+
+const findInvertedCase = word => {
+  const charArray = [...word];
+  return findIndex(charArray, (char, index) => {
+    const nextChar = charArray[index + 1];
+    if (!nextChar) {
+      return false;
+    } else if (char == char.toUpperCase()) {
+      return char == nextChar.toUpperCase() && nextChar == nextChar.toLowerCase();
+    } else {
+      return char == nextChar.toLowerCase() && nextChar == nextChar.toUpperCase();
+    }
+  });
+};
+
+const part1 = word => {
+  let index = findInvertedCase(word);
+  while (index > -1) {
+    let charArray = [...word];
+    charArray.splice(index, 2);
+    word = charArray.join('');
+    index = findInvertedCase(word);
+  }
+  return word.length;
 };
 const part2 = () => {
   return 0;
 };
-module.exports = { part1, part2 };
+module.exports = { findInvertedCase, part1, part2 };
