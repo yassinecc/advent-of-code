37a6a3c49b555740f037c5dde3cee0c8f52ece9c
Yassine Chbani <yassine.chbani@gmail.com>
2018-12-25 19:30:09 +0100

    Lighten test

--- 2018/src/day4/__tests__/day4.test.js
+++ 2018/src/day4/__tests__/day4.test.js
@@ -9,10 +9,10 @@ const {
 
 const log = [
   '[1518-11-01 00:00] Guard #10 begins shift',
-  '[1518-11-01 00:05] falls asleep',
+  '[1518-11-01 00:15] falls asleep',
   '[1518-11-01 00:25] wakes up',
   '[1518-11-01 00:30] falls asleep',
-  '[1518-11-01 00:55] wakes up',
+  '[1518-11-01 00:35] wakes up',
   '[1518-11-01 23:58] Guard #99 begins shift',
   '[1518-11-02 00:40] falls asleep',
   '[1518-11-02 00:50] wakes up',
@@ -67,56 +67,11 @@ describe('Day4', () => {
     expect(getSleepingHours(a, b)).toEqual(expected);
   });
 
+  const sleepLog = fillSleepLog(log);
   it('should correctly get the guards sleep times', () => {
-    expect(fillSleepLog(log)).toEqual({
+    expect(sleepLog).toEqual({
       '10': {
-        '1518-11-01': [
-          5,
-          6,
-          7,
-          8,
-          9,
-          10,
-          11,
-          12,
-          13,
-          14,
-          15,
-          16,
-          17,
-          18,
-          19,
-          20,
-          21,
-          22,
-          23,
-          24,
-          30,
-          31,
-          32,
-          33,
-          34,
-          35,
-          36,
-          37,
-          38,
-          39,
-          40,
-          41,
-          42,
-          43,
-          44,
-          45,
-          46,
-          47,
-          48,
-          49,
-          50,
-          51,
-          52,
-          53,
-          54,
-        ],
+        '1518-11-01': [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31, 32, 33, 34],
         '1518-11-03': [24, 25, 26, 27, 28],
       },
       '99': {
