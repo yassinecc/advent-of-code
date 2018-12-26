2355027d0a6e28801feca38d35509c70e49ce68d
Yassine Chbani <yassine.chbani@gmail.com>
2018-12-26 12:11:38 +0100

    Update index template

--- 2018/plop-templates/index.js
+++ 2018/plop-templates/index.js
@@ -1,3 +1,7 @@
-/* eslint-disable */
 const { parseFile } = require('../../utils/common');
 const { part1, part2 } = require('../{{fileName}}/parts');
+
+const input = parseFile('inputs/{{fileName}}.txt');
+
+console.log('Answer to part 1 is', part1(input));
+console.log('Answer to part 2 is', part2(input));
