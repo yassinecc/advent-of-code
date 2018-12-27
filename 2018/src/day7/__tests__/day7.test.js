64d55191d3af387b2ef200fe0df0262110b324bb
Yassine Chbani <yassine.chbani@gmail.com>
2018-12-27 11:20:37 +0100

    Start of day 7

---
+++ 2018/src/day7/__tests__/day7.test.js
@@ -0,0 +1,10 @@
+const { part1, part2 } = require('../parts');
+
+describe('Day 7', () => {
+  it('should solve part 1', () => {
+    expect(part1()).toEqual(0);
+  });
+  it('should solve part 2', () => {
+    expect(part2()).toEqual(0);
+  });
+});
