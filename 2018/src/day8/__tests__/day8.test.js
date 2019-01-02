da047c968704d261a5e06bbac613d5dd2e19df97
Yassine Chbani <yassine.chbani@gmail.com>
2019-01-02 12:22:21 +0100

    Fill metadata in nodes

--- 2018/src/day8/__tests__/day8.test.js
+++ 2018/src/day8/__tests__/day8.test.js
@@ -6,11 +6,11 @@ const file = input[0].split(' ').map(Number);
 
 describe('Day 8', () => {
   it('should correctly return for a leaf', () => {
-    expect(fillNode(file, 2)).toEqual(7);
-    expect(fillNode(file, 9)).toEqual(12);
+    expect(fillNode({}, file, 2)).toEqual(7);
+    expect(fillNode({}, file, 9)).toEqual(12);
   });
   it('should correctly return for a parent with a single leaf', () => {
-    expect(fillNode(file, 7)).toEqual(13);
+    expect(fillNode({}, file, 7)).toEqual(13);
   });
   it('should solve part 1', () => {
     expect(part1(input)).toEqual({ value: 138 });
--- 2018/src/day8/parts.js
+++ 2018/src/day8/parts.js
@@ -1,29 +1,42 @@
-const fillMetadata = (file, result, metadataIndex, numberOfMetadata) => {
-  [...Array(numberOfMetadata).keys()].forEach(index => {
-    result.value = result.value + file[metadataIndex + index];
+const Node = require('../../utils/Node');
+
+const fillMetadata = (file, result, metadataInfo, nodeInfo) => {
+  const { metadataIndex, number } = metadataInfo;
+  const { nodeCollection, nodeName } = nodeInfo;
+  [...Array(number).keys()].forEach(index => {
+    const metadataValue = file[metadataIndex + index];
+    nodeCollection[nodeName].metadata.push(metadataValue);
+    result.value += metadataValue;
   });
-  return metadataIndex + numberOfMetadata;
+  return metadataIndex + number;
 };
 
-const fillNode = (file, headerIndex, result = { value: 0 }) => {
+const fillNode = (nodeCollection, file, headerIndex, result = { value: 0 }, parentName) => {
+  const nodeNames = Object.keys(nodeCollection).map(Number);
+  const nextNodeName = nodeNames.length > 0 ? Math.max(...nodeNames) + 1 : 0;
+  Object.assign(nodeCollection, { [nextNodeName]: new Node(nextNodeName) });
   const numberOfChildren = file[headerIndex];
   const metadataIndex = headerIndex + 1;
   const numberOfMetadata = file[headerIndex + 1];
+  const nodeInfo = { nodeCollection, nodeName: nextNodeName };
   if (numberOfChildren === 0) {
-    return fillMetadata(file, result, metadataIndex + 1, numberOfMetadata);
+    const metadataInfo = { metadataIndex: metadataIndex + 1, number: numberOfMetadata };
+    return fillMetadata(file, result, metadataInfo, nodeInfo);
   }
   let childHeaderIndex = headerIndex + 2;
   [...Array(numberOfChildren).keys()].forEach(index => {
-    childHeaderIndex = fillNode(file, childHeaderIndex, result);
+    childHeaderIndex = fillNode(nodeCollection, file, childHeaderIndex, result);
   });
 
-  return fillMetadata(file, result, childHeaderIndex, numberOfMetadata);
+  const metadataInfo = { metadataIndex: childHeaderIndex, number: numberOfMetadata };
+  return fillMetadata(file, result, metadataInfo, nodeInfo);
 };
 
 const part1 = input => {
   const result = { value: 0 };
   const file = input[0].split(' ').map(Number);
-  fillNode(file, 0, result);
+  const nodeCollection = {};
+  fillNode(nodeCollection, file, 0, result);
   return result;
 };
 const part2 = () => 0;
