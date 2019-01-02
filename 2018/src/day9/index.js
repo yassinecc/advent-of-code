f1f8ee517f9ecea9a77f230cceb8742d02ca7358
Yassine Chbani <yassine.chbani@gmail.com>
2019-01-02 19:06:36 +0100

    Start of day 9

---
+++ 2018/src/day9/index.js
@@ -0,0 +1,7 @@
+const { parseFile } = require('../../utils/common');
+const { part1, part2 } = require('../day9/parts');
+
+const input = parseFile('inputs/day9.txt');
+
+console.log('Answer to part 1 is', part1(input));
+console.log('Answer to part 2 is', part2(input));
