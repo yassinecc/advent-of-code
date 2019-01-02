385a99fcea50a5f732802df7f9dacfc84ca63885
Yassine Chbani <yassine.chbani@gmail.com>
2019-01-02 22:02:20 +0100

    Add marbles

--- 2018/src/day9/__tests__/day9.test.js
+++ 2018/src/day9/__tests__/day9.test.js
@@ -1,6 +1,43 @@
-const { part1, part2 } = require('../parts');
+const { addMarble, part1, part2 } = require('../parts');
+
+const testArray = [
+  [0],
+  [0, 1],
+  [0, 2, 1],
+  [0, 2, 1, 3],
+  [0, 4, 2, 1, 3],
+  [0, 4, 2, 5, 1, 3],
+  [0, 4, 2, 5, 1, 6, 3],
+  [0, 4, 2, 5, 1, 6, 3, 7],
+  [0, 8, 4, 2, 5, 1, 6, 3, 7],
+];
+const indicesArray = [0, 1, 1, 3, 1, 3, 5, 7, 1];
 
 describe('Day 9', () => {
+  it('should correctly add standard marbles', () => {
+    let array = testArray[0];
+    let index = 0;
+    [...Array(8).keys()].forEach(idx => {
+      const result = addMarble(array, index, idx + 1);
+      array = result.array;
+      index = result.index;
+      expect(array).toEqual(testArray[idx + 1]);
+      expect(index).toEqual(indicesArray[idx + 1]);
+    });
+  });
+  const arr = [0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15];
+  it('should correctly add a 23rd marble', () => {
+    const xpected = [0, 16, 8, 17, 4, 18, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15];
+    expect(addMarble(arr, 13, 23)).toEqual({ array: xpected, index: 6 });
+  });
+  it('should correctly add a 23rd marble - negative index', () => {
+    const xpected = [0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 3, 14, 7, 15];
+    expect(addMarble(arr, 2, 23)).toEqual({ array: xpected, index: 18 });
+  });
+  it('should correctly add a 23rd marble - right edge', () => {
+    const xpected = [0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7];
+    expect(addMarble(arr, 6, 23)).toEqual({ array: xpected, index: 0 });
+  });
   it('should solve part 1', () => {
     expect(part1()).toEqual(0);
   });
--- 2018/src/day9/parts.js
+++ 2018/src/day9/parts.js
@@ -1,3 +1,18 @@
+const addMarble = (marbleArray, index, marble) => {
+  const newMarbleArray = [...marbleArray];
+  let insertionIndex;
+  if (marble % 23 !== 0) {
+    insertionIndex = ((index + 1) % marbleArray.length) + 1;
+    newMarbleArray.splice(insertionIndex, 0, marble);
+  } else {
+    indexToRemove = index - 7;
+    if (indexToRemove < 0) indexToRemove = marbleArray.length + indexToRemove;
+    insertionIndex = indexToRemove % (marbleArray.length - 1);
+    newMarbleArray.splice(indexToRemove, 1);
+  }
+  return { array: newMarbleArray, index: insertionIndex };
+};
+
 const part1 = () => 0;
 const part2 = () => 0;
-module.exports = { part1, part2 };
+module.exports = { addMarble, part1, part2 };
