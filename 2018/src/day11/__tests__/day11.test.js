935e9652748504af556a288f6f929b0bc5a7d941
Yassine Chbani <yassine.chbani@gmail.com>
2019-01-26 13:36:58 +0100

    Day 11 - energy calculation

--- 2018/src/day11/__tests__/day11.test.js
+++ 2018/src/day11/__tests__/day11.test.js
@@ -1,6 +1,18 @@
-const { part1, part2 } = require('../parts');
+const { getEnergyLevel, getAreaEnergyLevel, part1, part2 } = require('../parts');
 
 describe('Day 11', () => {
+  it.each([
+    [{ x: 3, y: 5 }, 8, 4],
+    [{ x: 122, y: 79 }, 57, -5],
+    [{ x: 217, y: 196 }, 39, 0],
+    [{ x: 101, y: 153 }, 71, 4],
+  ])('should find the correct energy', (c, s, r) => expect(getEnergyLevel(c, s)).toEqual(r));
+
+  it.each([[{ x: 33, y: 45 }, 18, 29], [{ x: 21, y: 61 }, 42, 30]])(
+      'should find the correct area energy',
+      (c, s, r) => expect(getAreaEnergyLevel(c, s)).toEqual(r)
+  );
+
   it('should solve part 1', () => {
     expect(part1()).toEqual(0);
   });
--- 2018/src/day11/parts.js
+++ 2018/src/day11/parts.js
@@ -1,3 +1,18 @@
+const getEnergyLevel = ({ x, y }, serialNumber) => {
+  const rackId = x + 10;
+  const int = (y * rackId + serialNumber) * rackId;
+  const hundreds = Math.floor(int / 100) % 10;
+  return hundreds - 5;
+};
+const getAreaEnergyLevel = ({ x, y }, serialNumber) => {
+  let result = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      result += getEnergyLevel({ x: x + i, y: y + j }, serialNumber);
+    }
+  }
+  return result;
+};
 const part1 = () => 0;
 const part2 = () => 0;
-module.exports = { part1, part2 };
+module.exports = { getEnergyLevel, getAreaEnergyLevel, part1, part2 };
