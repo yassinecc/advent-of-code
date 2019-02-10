df0cd433bf4e73d33d22d1c868adec350f651109
Yassine Chbani <yassine.chbani@gmail.com>
2019-02-10 19:58:58 +0100

    Define helpers

--- 2018/src/day12/__tests__/day12.test.js
+++ 2018/src/day12/__tests__/day12.test.js
@@ -1,6 +1,25 @@
-const { part1, part2 } = require('../parts');
+const { getInitialState, getSpreadRules, padInput, part1, part2 } = require('../parts');
 
 describe('Day 12', () => {
+  it('should get the initial state', () => {
+    const input = 'initial input: #..#..##.###.#..#.#';
+    expect(getInitialState(input)).toEqual('1001001101110100101');
+  });
+  it('should get spread rules', () => {
+    const input = `
+    initial input: ##.##..#..#.#.
+
+    ....# => #
+    ...#. => #
+    ..... => .
+    ...## => .
+
+    `;
+    expect(getSpreadRules(input)).toEqual(['0', '1', '1', '0']);
+  });
+  it('should pad input', () => {
+    expect(padInput('01100111000')).toEqual('00000110011100000');
+  });
   it('should solve part 1', () => {
     expect(part1()).toEqual(0);
   });
--- 2018/src/day12/parts.js
+++ 2018/src/day12/parts.js
@@ -1,3 +1,31 @@
+const { findRegex } = require('../../utils/common');
+
+const convertInput = input => input.replace(/#/g, 1).replace(/\./g, 0);
+
+const getInitialState = input => {
+  const convertedInput = convertInput(input);
+  return findRegex(convertedInput, /([10]+)/g);
+};
+
+const getSpreadRules = input => {
+  const convertedInput = convertInput(input);
+  const rules = findRegex(convertedInput, /([10]+ => [10])/g);
+  const result = [];
+  rules.forEach(rule => {
+    const [indexString, value] = rule.split(' => ');
+    const index = parseInt(indexString, 2);
+    result[index] = value;
+  });
+  return result;
+};
+
+const padInput = input => {
+  const start = input.indexOf('1');
+  const end = input.lastIndexOf('1');
+  return `00000${input.slice(start, end + 1)}00000`;
+};
+
 const part1 = () => 0;
 const part2 = () => 0;
-module.exports = { part1, part2 };
+
+module.exports = { getInitialState, getSpreadRules, padInput, part1, part2 };
