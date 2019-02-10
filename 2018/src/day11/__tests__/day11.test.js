aad45cee1f23390209aec02d0d5726483eb028bc
Yassine Chbani <yassine.chbani@gmail.com>
2019-02-10 13:52:40 +0100

    Use summed table

--- 2018/src/day11/__tests__/day11.test.js
+++ 2018/src/day11/__tests__/day11.test.js
@@ -1,4 +1,14 @@
-const { getEnergyLevel, getAreaEnergyLevel, part1, part2 } = require('../parts');
+const Matrix = require('vectorious').Matrix;
+const {
+  getEnergyLevel,
+  getAreaEnergyLevel,
+  getEnergyTable,
+  getSummedTable,
+  part1,
+  part2,
+} = require('../parts');
+
+getEdge = (point, cellSize) => ({ z: point.x + cellSize - 1, t: point.y + cellSize - 1 });
 
 describe('Day 11', () => {
   it.each([
@@ -6,13 +16,39 @@ describe('Day 11', () => {
     [{ x: 122, y: 79 }, 57, -5],
     [{ x: 217, y: 196 }, 39, 0],
     [{ x: 101, y: 153 }, 71, 4],
-  ])('should find the correct energy', (c, s, r) => expect(getEnergyLevel(c, s)).toEqual(r));
+  ])('should find the correct energy', (cell, serialNumber, result) => {
+    const point = { x: cell.x - 1, y: cell.y - 1 };
+    return expect(getEnergyLevel(point, serialNumber)).toEqual(result);
+  });
 
-  it.each([
-    [{ x: 33, y: 45 }, { z: 33 + 3, t: 45 + 3 }, 18, 29],
-    [{ x: 21, y: 61 }, { z: 21 + 3, t: 61 + 3 }, 42, 30],
-  ])('should find the correct area energy', (c, e, s, r) =>
-    expect(getAreaEnergyLevel(c, e, s)).toEqual(r)
+  it('should compute a summed table', () => {
+    const energyTable = new Matrix([[2, -3], [5, 7]]);
+    expect(getSummedTable(energyTable)).toEqual(new Matrix([[2, -1], [7, 11]]));
+  });
+
+  it.each([[{ x: 33, y: 45 }, 18, 29], [{ x: 21, y: 61 }, 42, 30]])(
+      'should find the correct area energy',
+      (cell, serialNumber, result) => {
+        const energyTable = getEnergyTable(serialNumber);
+        const summedTable = getSummedTable(energyTable);
+        const cellSize = 3;
+        const point = { x: cell.x - 1, y: cell.y - 1 };
+        const edge = getEdge(point, cellSize);
+
+        expect(getAreaEnergyLevel(summedTable, point, edge)).toEqual(result);
+      }
+  );
+
+  it.each([[{ x: 90, y: 269 }, 18, 16, 113], [{ x: 232, y: 251 }, 42, 12, 119]])(
+      'should find the correct max area energy',
+      (cell, serialNumber, size, result) => {
+        const point = { x: cell.x - 1, y: cell.y - 1 };
+        const edge = getEdge(point, size);
+        const energyTable = getEnergyTable(serialNumber);
+        const summedTable = getSummedTable(energyTable);
+
+        expect(getAreaEnergyLevel(summedTable, point, edge)).toEqual(result);
+      }
   );
 
   it('should solve part 1', () => {
--- 2018/src/day11/parts.js
+++ 2018/src/day11/parts.js
@@ -1,37 +1,85 @@
 const maxBy = require('lodash/maxBy');
+const Matrix = require('vectorious').Matrix;
 
 const gridSize = 300;
 
+const getEnergyTable = serialNumber => {
+  const energyTable = new Matrix(gridSize, gridSize);
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      energyTable.set(i, j, getEnergyLevel({ x: i, y: j }, serialNumber)); // Array starts at 1
+    }
+  }
+  return energyTable;
+};
+
+const safeMatrixAccess = (matrix, i, j) => {
+  const [xSize, ySize] = matrix.shape;
+  if (i < 0 || i >= xSize || j < 0 || j >= ySize) {
+    return 0;
+  }
+  return matrix.get(i, j);
+};
+
+const getSummedTable = energyTable => {
+  const tableSize = energyTable.shape[0];
+  const summedTable = new Matrix(tableSize, tableSize);
+  for (let i = 0; i < tableSize; i++) {
+    for (let j = 0; j < tableSize; j++) {
+      summedTable.set(
+          i,
+          j,
+          safeMatrixAccess(summedTable, i - 1, j) +
+          safeMatrixAccess(summedTable, i, j - 1) -
+          safeMatrixAccess(summedTable, i - 1, j - 1) +
+          energyTable.get(i, j)
+      );
+    }
+  }
+  return summedTable;
+};
+
 const getEnergyLevel = ({ x, y }, serialNumber) => {
+  x = x + 1; // Indices start at 1
+  y = y + 1;
   const rackId = x + 10;
   const int = (y * rackId + serialNumber) * rackId;
   const hundreds = Math.floor(int / 100) % 10;
   return hundreds - 5;
 };
-const getAreaEnergyLevel = ({ x, y }, { z, t }, serialNumber) => {
-  let result = 0;
-  for (let i = 0; i < z - x; i++) {
-    for (let j = 0; j < t - y; j++) {
-      result += getEnergyLevel({ x: x + i, y: y + j }, serialNumber);
-    }
-  }
-  return result;
-};
+
+const getAreaEnergyLevel = (summedTable, { x, y }, { z, t }) =>
+  safeMatrixAccess(summedTable, z, t) -
+  safeMatrixAccess(summedTable, x - 1, t) -
+  safeMatrixAccess(summedTable, z, y - 1) +
+  safeMatrixAccess(summedTable, x - 1, y - 1);
+
 const part1 = serialNumber => {
+  const energyTable = getEnergyTable(serialNumber);
+  const summedTable = getSummedTable(energyTable);
   const cellSize = 3;
   const array = [];
-  for (let i = 1; i < gridSize + 2 - cellSize; i++) {
-    for (let j = 1; j < gridSize + 2 - cellSize; j++) {
+  for (let i = 0; i < gridSize - cellSize; i++) {
+    for (let j = 0; j < gridSize - cellSize; j++) {
       const point = { x: i, y: j };
-      const edge = { z: i + cellSize, t: j + cellSize };
+      const edge = { z: i + cellSize - 1, t: j + cellSize - 1 };
       array.push({
         point,
-        value: getAreaEnergyLevel(point, edge, serialNumber),
+        value: getAreaEnergyLevel(summedTable, point, edge),
       });
     }
   }
-  const maxObject = maxBy(array, elt => elt.value);
-  return maxObject.point;
+  const { point } = maxBy(array, elt => elt.value);
+  return { x: point.x + 1, y: point.y + 1 };
 };
+
 const part2 = () => 0;
-module.exports = { getEnergyLevel, getAreaEnergyLevel, part1, part2 };
+
+module.exports = {
+  getEnergyLevel,
+  getAreaEnergyLevel,
+  getEnergyTable,
+  getSummedTable,
+  part1,
+  part2,
+};
