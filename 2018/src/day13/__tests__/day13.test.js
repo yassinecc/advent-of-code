91f022c0324de51f01c3c277315fd65cc92e3509
Yassine Chbani <yassine.chbani@gmail.com>
2019-02-16 14:14:55 +0100

    Parse tracks

--- 2018/src/day13/__tests__/day13.test.js
+++ 2018/src/day13/__tests__/day13.test.js
@@ -1,6 +1,104 @@
-const { part1, part2 } = require('../parts');
+const Matrix = require('vectorious').Matrix;
+const { parseTracks, part1, part2 } = require('../parts');
 
 describe('Day 13', () => {
+  it('should parse test tracks', () => {
+    const testTracks = String.raw`
+/->-\        
+|   |  /----\
+| /-+--+-\  |
+| | |  | v  |
+\-+-/  \-+--/
+  \------/
+`;
+    const testTrackLines = testTracks.split('\n').filter(track => track.length > 0);
+    expect(parseTracks(testTrackLines)).toEqual({
+      carts: [{ type: '>', x: 0, y: 2 }, { type: 'v', x: 3, y: 9 }],
+      tracksMatrix: new Matrix(
+          new Vector([
+            2,
+            0,
+            0,
+            0,
+            3,
+            -1,
+            -1,
+            -1,
+            -1,
+            -1,
+            -1,
+            -1,
+            -1,
+            1,
+            -1,
+            -1,
+            -1,
+            1,
+            -1,
+            -1,
+            2,
+            0,
+            0,
+            0,
+            0,
+            3,
+            1,
+            -1,
+            2,
+            0,
+            4,
+            0,
+            0,
+            4,
+            0,
+            3,
+            -1,
+            -1,
+            1,
+            1,
+            -1,
+            1,
+            -1,
+            1,
+            -1,
+            -1,
+            1,
+            -1,
+            1,
+            -1,
+            -1,
+            1,
+            3,
+            0,
+            4,
+            0,
+            2,
+            -1,
+            -1,
+            3,
+            0,
+            4,
+            0,
+            0,
+            2,
+            -1,
+            -1,
+            3,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            2,
+            0,
+            0,
+            0,
+          ]),
+          { shape: [6, 13] }
+      ),
+    });
+  });
   it('should solve part 1', () => {
     expect(part1()).toEqual(0);
   });
--- 2018/src/day13/parts.js
+++ 2018/src/day13/parts.js
@@ -1,3 +1,31 @@
+const Matrix = require('vectorious').Matrix;
+
+const cartCharacters = ['<', '>', '^', 'v'];
+
+const trackTypes = ['-', '|', '/', '\\', '+'];
+
+const findTrackType = (cartY, line) => {
+  if (['-', '+'].includes(line[cartY - 1]) || ['-', '+'].includes(line[cartY + 1])) {
+    return trackTypes.indexOf('-');
+  }
+  return trackTypes.indexOf('|');
+};
+
+const parseTracks = trackLines => {
+  const tracksMatrix = new Matrix(trackLines.length, trackLines[0].length);
+  const carts = [];
+  trackLines.forEach((line, i) => {
+    [...line].forEach((character, j) => {
+      if (!cartCharacters.includes(character)) {
+        tracksMatrix.set(i, j, trackTypes.indexOf(character));
+      } else {
+        carts.push({ x: i, y: j, type: character });
+        tracksMatrix.set(i, j, findTrackType(j, line));
+      }
+    });
+  });
+  return { tracksMatrix, carts };
+};
 const part1 = () => 0;
 const part2 = () => 0;
-module.exports = { part1, part2 };
+module.exports = { parseTracks, part1, part2 };
