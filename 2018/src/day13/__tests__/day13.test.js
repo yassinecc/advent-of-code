f8ff22f0c1c51a123fde7aaedd03fada43521ba9
Yassine Chbani <yassine.chbani@gmail.com>
2019-02-17 00:36:04 +0100

    Move carts

--- 2018/src/day13/__tests__/day13.test.js
+++ 2018/src/day13/__tests__/day13.test.js
@@ -1,5 +1,92 @@
 const Matrix = require('vectorious').Matrix;
-const { parseTracks, part1, part2 } = require('../parts');
+const { parseTracks, advanceCart, part1, part2 } = require('../parts');
+
+const initialTestCase = {
+  carts: [{ type: '>', x: 0, y: 2, turnFlag: 0 }, { type: 'v', x: 3, y: 9, turnFlag: 0 }],
+  tracksMatrix: new Matrix(
+      new Vector([
+        2,
+        0,
+        0,
+        0,
+        3,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        1,
+        -1,
+        -1,
+        -1,
+        1,
+        -1,
+        -1,
+        2,
+        0,
+        0,
+        0,
+        0,
+        3,
+        1,
+        -1,
+        2,
+        0,
+        4,
+        0,
+        0,
+        4,
+        0,
+        3,
+        -1,
+        -1,
+        1,
+        1,
+        -1,
+        1,
+        -1,
+        1,
+        -1,
+        -1,
+        1,
+        -1,
+        1,
+        -1,
+        -1,
+        1,
+        3,
+        0,
+        4,
+        0,
+        2,
+        -1,
+        -1,
+        3,
+        0,
+        4,
+        0,
+        0,
+        2,
+        -1,
+        -1,
+        3,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        2,
+        0,
+        0,
+        0,
+      ]),
+      { shape: [6, 13] }
+  ),
+};
 
 describe('Day 13', () => {
   it('should parse test tracks', () => {
@@ -12,92 +99,32 @@ describe('Day 13', () => {
   \------/
 `;
     const testTrackLines = testTracks.split('\n').filter(track => track.length > 0);
-    expect(parseTracks(testTrackLines)).toEqual({
-      carts: [{ type: '>', x: 0, y: 2 }, { type: 'v', x: 3, y: 9 }],
-      tracksMatrix: new Matrix(
-          new Vector([
-            2,
-            0,
-            0,
-            0,
-            3,
-            -1,
-            -1,
-            -1,
-            -1,
-            -1,
-            -1,
-            -1,
-            -1,
-            1,
-            -1,
-            -1,
-            -1,
-            1,
-            -1,
-            -1,
-            2,
-            0,
-            0,
-            0,
-            0,
-            3,
-            1,
-            -1,
-            2,
-            0,
-            4,
-            0,
-            0,
-            4,
-            0,
-            3,
-            -1,
-            -1,
-            1,
-            1,
-            -1,
-            1,
-            -1,
-            1,
-            -1,
-            -1,
-            1,
-            -1,
-            1,
-            -1,
-            -1,
-            1,
-            3,
-            0,
-            4,
-            0,
-            2,
-            -1,
-            -1,
-            3,
-            0,
-            4,
-            0,
-            0,
-            2,
-            -1,
-            -1,
-            3,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            2,
-            0,
-            0,
-            0,
-          ]),
-          { shape: [6, 13] }
-      ),
-    });
+    expect(parseTracks(testTrackLines)).toEqual(initialTestCase);
+  });
+  it('should advance carts', () => {
+    const { carts } = initialTestCase;
+
+    const cart11 = advanceCart(carts[0], '-');
+    const cart21 = advanceCart(carts[1], '|');
+    const expectedCart11 = { type: '>', x: 0, y: 3, turnFlag: 0 };
+    const expectedCart21 = { type: 'v', x: 4, y: 9, turnFlag: 0 };
+    expect(cart11).toEqual(expectedCart11);
+    expect(cart21).toEqual(expectedCart21);
+
+    const cart12 = advanceCart(expectedCart11, '-');
+    const cart22 = advanceCart(expectedCart21, '+');
+    const expectedCart12 = { type: '>', x: 0, y: 4, turnFlag: 0 };
+    const expectedCart22 = { type: '>', x: 4, y: 10, turnFlag: 1 };
+    expect(cart12).toEqual(expectedCart12);
+    expect(cart22).toEqual(expectedCart22);
+    expect(cart21).toEqual(expectedCart21);
+
+    const cart13 = advanceCart(expectedCart12, '\\');
+    const cart23 = advanceCart(expectedCart22, '-');
+    const expectedCart13 = { type: 'v', x: 1, y: 4, turnFlag: 0 };
+    const expectedCart23 = { type: '>', x: 4, y: 11, turnFlag: 1 };
+    expect(cart13).toEqual(expectedCart13);
+    expect(cart23).toEqual(expectedCart23);
   });
   it('should solve part 1', () => {
     expect(part1()).toEqual(0);
--- 2018/src/day13/parts.js
+++ 2018/src/day13/parts.js
@@ -1,6 +1,8 @@
 const Matrix = require('vectorious').Matrix;
 
-const cartCharacters = ['<', '>', '^', 'v'];
+const cartCharacters = ['<', '^', '>', 'v'];
+
+const cartCharToDirection = { '<': 'left', '^': 'up', '>': 'right', "v": 'down' };
 
 const trackTypes = ['-', '|', '/', '\\', '+'];
 
@@ -19,13 +21,79 @@ const parseTracks = trackLines => {
       if (!cartCharacters.includes(character)) {
         tracksMatrix.set(i, j, trackTypes.indexOf(character));
       } else {
-        carts.push({ x: i, y: j, type: character });
+        // turnFlag is 0 when next turn is left,
+        // 1 when next is straight
+        // 2 if right
+        carts.push({ x: i, y: j, type: character, turnFlag: 0 });
         tracksMatrix.set(i, j, findTrackType(j, line));
       }
     });
   });
   return { tracksMatrix, carts };
 };
+
+const getIntersectionChoice = cart => {
+  // Subtract one because turnFlag is 0 to go left
+  const characterIndex = (cartCharacters.indexOf(cart.type) + cart.turnFlag - 1) % 3;
+  return cartCharToDirection[cartCharacters[characterIndex]];
+};
+
+const moveCart = (cart, direction) => {
+  const { x, y } = cart;
+  switch (direction) {
+    case 'up':
+      return { ...cart, type: '^', x: x - 1 };
+    case 'down':
+      return { ...cart, type: 'v', x: x + 1 };
+    case 'left':
+      return { ...cart, type: '<', y: y - 1 };
+    case 'right':
+      return { ...cart, type: '>', y: y + 1 };
+    default:
+      return cart;
+  }
+};
+
+const advanceCart = (cart, currentTrackCharacter) => {
+  const { type } = cart;
+  switch (currentTrackCharacter) {
+    case '-':
+      if (type === '>') {
+        return moveCart(cart, 'right');
+      } else return moveCart(cart, 'left');
+    case '|':
+      if (type === 'v') {
+        return moveCart(cart, 'down');
+      } else return moveCart(cart, 'up');
+    case '/':
+      if (type === '<') {
+        return moveCart(cart, 'down');
+      } else if (type === '>') {
+        return moveCart(cart, 'up');
+      } else if (type === 'v') {
+        return moveCart(cart, 'left');
+      } else {
+        return moveCart(cart, 'right'); // ^ case
+      }
+    case '\\':
+      if (type === '<') {
+        return moveCart(cart, 'up');
+      } else if (type === '>') {
+        return moveCart(cart, 'down');
+      } else if (type === 'v') {
+        return moveCart(cart, 'right');
+      } else {
+        return moveCart(cart, 'left'); // ^ case
+      }
+    case '+':
+      const interSectionChoice = getIntersectionChoice(cart);
+      const newTurnFlag = (cart.turnFlag + 1) % 3;
+      return moveCart({ ...cart, turnFlag: newTurnFlag }, interSectionChoice);
+    default:
+      return cart;
+  }
+};
+
 const part1 = () => 0;
 const part2 = () => 0;
-module.exports = { parseTracks, part1, part2 };
+module.exports = { parseTracks, advanceCart, part1, part2 };
