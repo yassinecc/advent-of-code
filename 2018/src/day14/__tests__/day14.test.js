6d06fc4c61d1836a45ddcb08567e909d65a25b80
Yassine Chbani <yassine.chbani@gmail.com>
2019-02-17 14:25:29 +0100

    :sparkles: (day14) part 2

--- 2018/src/day14/__tests__/day14.test.js
+++ 2018/src/day14/__tests__/day14.test.js
@@ -8,6 +8,9 @@ describe('Day 14', () => {
     expect(part1(2018)).toEqual('5941429882');
   });
   it('should solve part 2', () => {
-    expect(part2()).toEqual(0);
+    expect(part2('51589')).toEqual(9);
+    expect(part2('01245')).toEqual(5);
+    expect(part2('92510')).toEqual(18);
+    expect(part2('59414')).toEqual(2018);
   });
 });
--- 2018/src/day14/index.js
+++ 2018/src/day14/index.js
@@ -4,4 +4,4 @@ const { part1, part2 } = require('../day14/parts');
 const input = parseFile('inputs/day14.txt');
 
 console.log('Answer to part 1 is', part1(input[0]));
-console.log('Answer to part 2 is', part2(input));
+console.log('Answer to part 2 is', part2(input[0]));
