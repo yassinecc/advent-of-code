e5cd0f62fb496cf4ea1f3b97f91bde5bafdb9edc
Yassine Chbani <yassine.chbani@gmail.com>
2019-03-02 22:20:15 +0100

    :sparkles: (Day15) Move in turns

--- 2018/src/day15/__tests__/day15.test.js
+++ 2018/src/day15/__tests__/day15.test.js
@@ -1,14 +1,40 @@
-const { parse2dArray, getNextSteps, getShortestPath, part1, part2 } = require('../parts');
+const {
+  parse2dArray,
+  parseMap,
+  getNextSteps,
+  getShortestPath,
+  playRound,
+  part1,
+  part2,
+} = require('../parts');
 
 const testInput = ['#######', '#E....#', '#...#.#', '#.G.#G#', '#######'];
 
+const testInput2 = [
+  '#########',
+  '#G..G..G#',
+  '#.......#',
+  '#.......#',
+  '#G..E..G#',
+  '#.......#',
+  '#.......#',
+  '#G..G..G#',
+  '#########',
+];
+
 const testMap = parse2dArray(testInput);
 
+const testMap2 = parse2dArray(testInput2);
+
 describe('Day 15', () => {
+  it('should parse maps', () => {
+    expect(parseMap(testMap)).toEqual(testInput);
+    expect(parseMap(testMap2)).toEqual(testInput2);
+  });
   it('should get next steps', () => {
-    expect(getNextSteps({ x: 1, y: 1 }, testMap)).toEqual([{ x: 1, y: 2 }, { x: 2, y: 1 }]);
-    expect(getNextSteps({ x: 1, y: 4 }, testMap)).toEqual([{ x: 1, y: 3 }, { x: 1, y: 5 }]);
-    expect(getNextSteps({ x: 3, y: 5 }, testMap)).toEqual([{ x: 2, y: 5 }]);
+    expect(getNextSteps('G', { x: 1, y: 1 }, testMap)).toEqual([{ x: 1, y: 2 }, { x: 2, y: 1 }]);
+    expect(getNextSteps('G', { x: 1, y: 4 }, testMap)).toEqual([{ x: 1, y: 3 }, { x: 1, y: 5 }]);
+    expect(getNextSteps('G', { x: 3, y: 5 }, testMap)).toEqual([{ x: 2, y: 5 }]);
   });
   it('should get the shortest path', () => {
     expect(getShortestPath({ x: 1, y: 1 }, testMap)).toEqual({
@@ -16,6 +42,44 @@ describe('Day 15', () => {
       y: 2,
       path: [{ x: 1, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 2 }],
     });
+  });
+  it('should play rounds', () => {
+    playRound(testMap2);
+    expect(parseMap(testMap2)).toEqual([
+      '#########',
+      '#.G...G.#',
+      '#...G...#',
+      '#...E..G#',
+      '#.G.....#',
+      '#.......#',
+      '#G..G..G#',
+      '#.......#',
+      '#########',
+    ]);
+    playRound(testMap2);
+    expect(parseMap(testMap2)).toEqual([
+      '#########',
+      '#..G.G..#',
+      '#...G...#',
+      '#.G.E.G.#',
+      '#.......#',
+      '#G..G..G#',
+      '#.......#',
+      '#.......#',
+      '#########',
+    ]);
+    playRound(testMap2);
+    expect(parseMap(testMap2)).toEqual([
+      '#########',
+      '#.......#',
+      '#..GGG..#',
+      '#..GEG..#',
+      '#G..G...#',
+      '#......G#',
+      '#.......#',
+      '#.......#',
+      '#########',
+    ]);
   });
   it('should solve part 1', () => {
     expect(part1()).toEqual(0);
--- 2018/src/day15/parts.js
+++ 2018/src/day15/parts.js
@@ -1,3 +1,5 @@
+const PLAYER_TYPES = ['E', 'G'];
+
 const parse2dArray = input => {
   const result = [];
   [...Array(input.length).keys()].forEach(_ => result.push([]));
@@ -7,13 +9,16 @@ const parse2dArray = input => {
   return result;
 };
 
-const getNextSteps = (spot, map) => {
+const parseMap = playerMap =>
+  playerMap.map(playerMapRow => playerMapRow.reduce((acc, player) => acc + player.type, ''));
+
+const getNextSteps = (opponentType, spot, map) => {
   const results = [];
   const increments = [{ i: -1, j: 0 }, { i: 0, j: -1 }, { i: 0, j: 1 }, { i: 1, j: 0 }];
   increments.forEach(increment => {
     const potential = { x: spot.x + increment.i, y: spot.y + increment.j };
     const { type } = map[potential.x][potential.y];
-    if (type !== '#') results.push(potential);
+    if (type === '.' || type === opponentType) results.push(potential);
   });
   return results;
 };
@@ -38,9 +43,10 @@ const getShortestPath = (player, map) => {
     const currentSpot = queue.shift();
     const currentType = map[currentSpot.x][currentSpot.y].type;
     if (currentType === opponentType) {
+      map.forEach((_, i) => _.forEach(({ type }, j) => (map[i][j] = { type })));
       return currentSpot;
     }
-    const nextSteps = getNextSteps(currentSpot, map);
+    const nextSteps = getNextSteps(opponentType, currentSpot, map);
     nextSteps.forEach(step => {
       const element = map[step.x][step.y];
       if (!element.checked) {
@@ -52,8 +58,37 @@ const getShortestPath = (player, map) => {
       }
     });
   }
+  return { ...player, path: [] };
+};
+
+const swapCells = (map, firstCell, secondCell) => {
+  const nextCellValue = map[secondCell.x][secondCell.y];
+  map[secondCell.x][secondCell.y] = map[firstCell.x][firstCell.y];
+  map[firstCell.x][firstCell.y] = nextCellValue;
+};
+
+const playTurn = (player, map) => {
+  const { path } = getShortestPath(player, map);
+  // Not next to opponent
+  if (path.length > 1) {
+    const nextCell = path[1];
+    swapCells(map, player, nextCell);
+  }
+};
+
+const playRound = map => {
+  const players = [];
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      const cell = map[i][j];
+      if (PLAYER_TYPES.includes(cell.type)) {
+        players.push({ x: i, y: j });
+      }
+    }
+  }
+  players.forEach(player => playTurn(player, map));
 };
 
 const part1 = () => 0;
 const part2 = () => 0;
-module.exports = { parse2dArray, getNextSteps, getShortestPath, part1, part2 };
+module.exports = { parse2dArray, parseMap, getNextSteps, getShortestPath, playRound, part1, part2 };
