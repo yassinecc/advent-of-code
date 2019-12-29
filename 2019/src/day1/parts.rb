87bd2adfc5797de876fc0b697f9202a3c1ae64b9
Yassine Chbani <yassine.chbani@gmail.com>
2019-12-29 11:54:20 +0100

    :sparkles: (2019/day 1) part 2

--- 2019/src/day1/parts.rb
+++ 2019/src/day1/parts.rb
@@ -2,6 +2,21 @@ def get_single_fuel(mass)
     return (mass.to_i / 3).floor - 2
 end
 
+def get_composite_fuel(fuel)
+    result = 0
+    extra = fuel
+    while(get_single_fuel(extra) > 0)
+        extraFuel = get_single_fuel(extra)
+        result += extraFuel
+        extra = extraFuel
+    end
+    return result
+end
+
 def part1(args)
     return args.map{|x| get_single_fuel(x)}.sum
+end
+
+def part2(args)
+    return args.map{|x| get_composite_fuel(x)}.sum
 end
--- 2019/src/spec/day1_spec.rb
+++ 2019/src/spec/day1_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe "Day 1" do
         expect(part1([1969])).to eq 654
         expect(part1([100756])).to eq 33583
     end
+
+    it "solves part 2" do
+        expect(part2([14])).to eq 2
+        expect(part2([1969])).to eq 966
+        expect(part2([100756])).to eq 50346
+    end
 end
