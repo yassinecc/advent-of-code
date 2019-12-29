e6b39d8753e7ec6b806ff61c480bfb3a9bb9714b
Yassine Chbani <yassine.chbani@gmail.com>
2019-12-29 11:44:59 +0100

    :recycle: (2019/day 1) Extract method

--- 2019/src/day1/parts.rb
+++ 2019/src/day1/parts.rb
@@ -1,3 +1,7 @@
+def get_single_fuel(mass)
+    return (mass.to_i / 3).floor - 2
+end
+
 def part1(args)
-    return args.map{|x| (x.to_i / 3).floor - 2 }.sum
+    return args.map{|x| get_single_fuel(x)}.sum
 end
