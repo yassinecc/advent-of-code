2b84d01f8bd9df255972e859b9f5e57cf32e0361
Yassine Chbani <31844581+yassinecc@users.noreply.github.com>
2020-12-26 13:24:01 +0100

    Use list comprehension

--- 2020/aoc_2020/index.py
+++ 2020/aoc_2020/index.py
@@ -3,6 +3,7 @@
 import utils
 import day1
 
-input = list(map(lambda str: int(str), utils.read_file("./inputs/day1.txt")))
+file_input = utils.read_file("./inputs/day1.txt")
+input = [int(line) for line in file_input]
 print(day1.part1(input, 2020))
 print(day1.part2(input, 2020))
