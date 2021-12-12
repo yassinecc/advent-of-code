919fe1d186152e0d82e26e5b14cadca4379792a2
Yassine Chbani <31844581+yassinecc@users.noreply.github.com>
2021-12-12 23:47:47 +0100

    Refactor tests

--- 2021/spec/day2_spec.rb
+++ 2021/spec/day2_spec.rb
@@ -2,24 +2,22 @@ require 'rspec'
 require_relative '../src/day2'
 
 describe 'Day2' do
+  let(:input) do
+    ['forward 5',
+     'down 5',
+     'forward 8',
+     'up 3',
+     'down 8',
+     'forward 2']
+  end
   context 'part 1' do
     it 'succeeds' do
-      expect(Day2.new.part1(['forward 5',
-                             'down 5',
-                             'forward 8',
-                             'up 3',
-                             'down 8',
-                             'forward 2'])).to eq(150)
+      expect(Day2.new.part1(input)).to eq(150)
     end
   end
   context 'part 2' do
     it 'succeeds' do
-      expect(Day2.new.part2(['forward 5',
-                             'down 5',
-                             'forward 8',
-                             'up 3',
-                             'down 8',
-                             'forward 2'])).to eq(900)
+      expect(Day2.new.part2(input)).to eq(900)
     end
   end
 end
